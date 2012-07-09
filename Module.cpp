eae7209431b1fcf56c6576a3255fa1ceb6dcfa18
MusashiNakajima <634.nakajima@gmail.com>
2012-07-09 23:19:23 +0900

    en1

--- Module.cpp
+++ Module.cpp
@@ -233,7 +233,7 @@ void Module::getAddr()
     
     /* eth0のIPアドレスを取得 */
 
-    strncpy(ifr.ifr_name, "en0", IFNAMSIZ-1);
+    strncpy(ifr.ifr_name, "en1", IFNAMSIZ-1);
 
     ioctl(fd, SIOCGIFADDR, &ifr);
     
