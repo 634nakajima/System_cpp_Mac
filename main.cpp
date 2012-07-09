ceb1b83036853600ff2cdb40f49ab7e3105314c5
MusashiNakajima <634.nakajima@gmail.com>
2012-07-09 22:59:37 +0900

    test

--- main.cpp
+++ main.cpp
@@ -33,7 +33,11 @@ int main()
     
     env->sendSetMdtkn();
     getchar();
-    co->connect(7, 2, "/Stream");
+    co->connect(6, 2, "/Stream");
+    co->connect(2, 3, "/Stream");
+    co->connect(4, 2, "/Data");
+
+
     getchar();
 	delete dac;
 	delete adc;
